579804c2808cbc5badd2e8c1047769fc706509ca
Adil Bel Mqadem <abel-mqa@e2r5p6.1337.ma>
2025-01-25 20:04:43 +0100

    abel-mqa branch created

--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,5 @@ int main(int ac, char **av)
 {
 	(void)ac, (void)av;
 
-	std::cout << "Hello Team,\nThis marks the beginning of our journey together. Welcome aboard, and cheers to success!" << std::endl;
+	std::cout << "This is abel-mqa branch" << std::endl;
 }
