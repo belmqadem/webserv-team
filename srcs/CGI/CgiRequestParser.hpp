29b608baff5bdaca2d6463c66d0d6fa8c1c9934a
Mohammed Chihab <mchihab@c3r6p3.1337.ma>
2025-02-20 17:49:02 +0100

    Lets Start

---
+++ srcs/CGI/CgiRequestParser.hpp
@@ -0,0 +1,19 @@
+#include "../../includes/webserv.hpp"
+
+
+class CgiRequestParser
+{
+private:
+    /* data */
+public:
+    CgiRequestParser(/* args */);
+    ~CgiRequestParser();
+};
+
+CgiRequestParser::CgiRequestParser(/* args */)
+{
+}
+
+CgiRequestParser::~CgiRequestParser()
+{
+}
