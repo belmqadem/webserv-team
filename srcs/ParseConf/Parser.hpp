a8923601485d59f5e844639c46c4aedf011768e7
NourMellal <mellal.nour9@gmail.com>
2025-02-28 02:17:30 +0000

    Implement parsing for additional configuration directives and update token types ; version 0.1

--- srcs/ParseConf/Parser.hpp
+++ srcs/ParseConf/Parser.hpp
@@ -2,15 +2,22 @@
 
 #include "Tokenize.hpp"
 
-class Parser {
-    private:
-        std::vector<Token>  _tokens;
-        size_t              _index;
-    public:
-        Parser(std::vector<Token> t) : _tokens(t), _index(0) {}
-        Token consume(TokenType expected);
-        
-        void    parseDirecive();
-        void    parseServerBlock();
-        void    parseConfig();
+class Parser
+{
+private:
+    std::vector<Token> _tokens;
+    size_t _index;
+
+public:
+    Parser(std::vector<Token> t) : _tokens(t), _index(0) {}
+    Token consume(TokenType expected);
+
+    void parseDirecive();
+    void parseListenDirective();
+    void parseServerNameDirective();
+    void parseRootDirective();
+    void parseErrorPageDirective();
+    void parseLocationBlock();
+    void parseServerBlock();
+    void parseConfig();
 };
