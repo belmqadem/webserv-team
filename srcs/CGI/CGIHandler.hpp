a1bfbfebdbd61a78e753c535fe734749e8366269
Mohammed Chihab <mchihab@c3r2p2.1337.ma>
2025-03-04 14:16:09 +0000

    gd

--- srcs/CGI/CGIHandler.hpp
+++ srcs/CGI/CGIHandler.hpp
@@ -6,11 +6,16 @@
 class CGIHandler {
 private:
     std::string scriptPath;
-    char *envp[5];  // Static environment variables for now
+    char *envp[5];
+    std::string path;
 
 public:
+    CGIHandler();
     CGIHandler(std::string path);
+    CGIHandler(std::string pathS, std::string pathE);
+    
     void setupEnvironment();
+    char **setupExecveArgs(const std::string& path);
     void execute();
 };
 
