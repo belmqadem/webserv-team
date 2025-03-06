2c1f589d15504d7e4956fa6a026f1692bd8674de
NourMellal <mellal.nour9@gmail.com>
2025-03-06 17:28:47 +0000

    Add IOMultiplexer and exception handling for event-driven server architecture

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,13 +1,29 @@
 #include "../includes/webserv.hpp"
+#include "server/IOMultiplexer.hpp"
+#include <signal.h>
+
+extern int webserv_signal;
+
+void	sigint_handle(int sig) {
+	webserv_signal = sig;
+	std::cout << "Interrupt signal catched." << std::endl;
+}
+
+void signalhandler() {
+	if (signal(SIGQUIT, SIG_IGN) == SIG_ERR)
+		throw std::runtime_error("signal() faild.");
+	if (signal(SIGINT, &sigint_handle) == SIG_ERR)
+		throw std::runtime_error("signal() faild.");
+}
 
 int main(int ac, char **av)
 {
 	(void)ac, (void)av;
 
-	std::cout << "Hello Team,\nThis marks the beginning of our journey together. Welcome aboard, and cheers to success!" << std::endl;
 	try {
-		std::cout << "hello" << std::endl;
-	} catch (...) {
-		throw "helloss";
+		//server.start();
+		IOMultiplexer::getInstance().runEventLoop();
+	} catch (std::exception &e) {
+		std::cerr << "Fatal error: \n" << e.what() << "\n";
 	}
 }
