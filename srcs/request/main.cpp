114d17fff4f158dd874d15ccb9a0285524d3a6c9
Mohammed Chihab <mchihab@c3r7p3.1337.ma>
2025-03-06 02:26:13 +0000

    ok

--- srcs/request/main.cpp
+++ srcs/request/main.cpp
@@ -3,20 +3,16 @@
 
 int main()
 {
-    // Sample HTTP request
     std::string request = 
-        "GET /a.py?name=world HTTP/1.1\r\n"
-        "Host: a.py\r\n"
+        "GET /hello?name=world HTTP/1.1\r\n"
+        "Host: example.com\r\n"
         "Connection: keep-alive\r\n"
         "Content-Type: text/plain\r\n"
         "Content-Length: 5\r\n"
         "\r\n"
         "Hello";
-    // Create a RequestParser object
     RequestParser parser(request);
     CGIHandler cgi;
     cgi.executeCGI(parser);
-    // Print the parsed request
-    parser.print_request();
     return 0;
 }
