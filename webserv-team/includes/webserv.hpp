3a4dc5c1adacb6b4145cd64268738b6b1b9118b0
NourMellal <mellal.nour9@gmail.com>
2025-03-10 07:09:42 +0000

    Add initial implementation of Logger, ConfigManager, IOMultiplexer, and ClientServer classes; create corresponding header files and update Makefile

---
+++ webserv-team/includes/webserv.hpp
@@ -0,0 +1,15 @@
+// filepath: /home/nmellal/Projects/webserv-team/includes/webserv.hpp
+#ifndef WEBSERV_HPP
+#define WEBSERV_HPP
+
+#include <vector>
+#include <string>
+#include "Server.hpp"
+#include "ClientServer.hpp"
+#include "ConfigManager.hpp"
+#include "IOMultiplexer.hpp"
+#include "Logger.hpp"
+
+// Define any necessary constants or types here
+
+#endif // WEBSERV_HPP
