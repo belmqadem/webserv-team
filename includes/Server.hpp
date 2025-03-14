2193359ec5f621c000af4ceb725f38f88494ac13
Adil Bel Mqadem <abel-mqa@e2r5p6.1337.ma>
2025-03-14 04:01:49 +0000

    merging ConfigManager with RequestParser

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -5,6 +5,7 @@
 #include "ConfigManager.hpp"
 #include "IOMultiplexer.hpp"
 #include "webserv.hpp"
+#include "ClientServer.hpp"
 
 #define RD_SIZE 1024
 
@@ -15,41 +16,6 @@ std::string to_string(T t)
 	str << t;
 	return str.str();
 }
-
-class ClientServer : IEvenetListeners
-{
-private:
-	bool _is_started;
-	int _server_socket_fd;
-	int _peer_socket_fd;
-	epoll_event _epoll_ev;
-	sockaddr_in _client_addr;
-	RequestParser request;
-	std::string _request_buffer;
-	std::string _response_buffer;
-
-public:
-	/* getters and setters */
-	bool isStarted() const;
-	void setPeerSocketFd(uint32_t fd);
-	void setServerSocketFd(uint32_t fd);
-	void setClientAddr(sockaddr_in addr);
-
-public:
-	void RegisterWithIOMultiplexer();
-
-	ClientServer(const int &server_socket_fd, const int &peer_socket_fd);
-	~ClientServer();
-
-	virtual void terminate();
-
-	virtual void onEvent(int fd, epoll_event ev);
-
-private:
-	void handleIncomingData();
-	void handleResponse();
-	void enableWriteEvent();
-};
 
 class Server : public IEvenetListeners
 {
@@ -79,4 +45,16 @@ public:
 
 	virtual void terminate();
 	virtual void onEvent(int fd, epoll_event ev);
+
+	const ServerConfig *getMatchingServerConfig(const std::string &host) const
+	{
+		for (size_t i = 0; i < _config.size(); i++)
+		{
+			if (_config[i].serverNames[0] == host)
+			{
+				return &_config[i];
+			}
+		}
+		return &_config[0];
+	}
 };
