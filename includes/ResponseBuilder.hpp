19d6d5b2cb7a7b8511fa1516c6d341a47dc859ff
valarmo <adilfront3@gmail.com>
2025-03-28 01:40:41 +0000

    code optimization (request & response)

--- includes/ResponseBuilder.hpp
+++ includes/ResponseBuilder.hpp
@@ -6,16 +6,16 @@
 class ResponseBuilder
 {
 private:
-	std::string response; // The html response that will be sent to the client
+	RequestParser request; // The request that will be processed
+	std::string response;  // The response that will be sent to the client
 	std::string http_version;
 	std::string status;
 	std::map<std::string, std::string> headers;
 	std::string body;
 	short status_code;
 	std::map<std::string, void (ResponseBuilder::*)(void)> routes; // A map to route the request to the correct method
-	RequestParser request;
-	const ServerConfig *server_config; // Pointer to the matched server block
-	const Location *location_config;   // Pointer to the matched location block
+	const ServerConfig *server_config;							   // Pointer to the matched server block
+	const Location *location_config;							   // Pointer to the matched location block
 
 	// Required HTTP Methods
 	void doGET();
@@ -26,7 +26,8 @@ private:
 	void init_config();
 	void init_routes();
 	bool handle_redirection();
-	std::string generate_error_page(short status_code);
+	std::string generate_error_page();
+	std::string generate_default_root();
 	std::string generate_directory_listing(const std::string &path);
 	std::string generate_response_string();
 	std::string detect_mime_type(const std::string &path);
@@ -36,10 +37,7 @@ private:
 	std::string read_html_file(const std::string &filename);
 	std::string generate_upload_success_page(const std::string &filename);
 
-	// Map to save the mime types
 	static std::map<std::string, std::string> mime_types;
-
-	// Method to initialize the mime types
 	static std::map<std::string, std::string> init_mime_types();
 
 	ResponseBuilder(const ResponseBuilder &);
@@ -47,10 +45,11 @@ private:
 
 public:
 	ResponseBuilder(RequestParser &request);
-	std::map<std::string, std::string> prepareEnv( RequestParser &request) const ;
+
+	// Main Function that builds the response
+	std::string build_response();
 
 	// Setters
-	void set_http_version(const std::string &http_version);
 	void set_status(short status_code);
 	void set_headers(const std::string &key, const std::string &value);
 	void set_body(const std::string &body);
@@ -63,7 +62,4 @@ public:
 	std::string get_header_value(std::string &key);
 	std::string get_body();
 	short get_status_code();
-
-	// Main Function that builds the response
-	std::string build_response();
 };
