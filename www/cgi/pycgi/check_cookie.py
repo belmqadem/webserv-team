d4cc565fa0b3d8538ad25b102ded616df59e5362
belmqadem <adilfront3@gmail.com>
2025-04-10 21:03:07 +0100

    change khfif

--- www/cgi/pycgi/check_cookie.py
+++ www/cgi/pycgi/check_cookie.py
@@ -2,19 +2,16 @@
 
 import os
 
-print("Content-Type: text/html")
-print()
+print("Content-Type: text/html\n")
+print("<html><body><h2>Cookies</h2>")
 
-cookies = os.environ.get("HTTP_COOKIE", "")
-
-html = "<html><body><h2>Cookies</h2><ul>"
-
-if cookies:
-    for pair in cookies.split("; "):
-        html += f"<li>{pair}</li>"
+cookie = os.environ.get("HTTP_COOKIE")
+if cookie:
+    print("<pre>")
+    for c in cookie.split("; "):
+        print(c)
+    print("</pre>")
 else:
-    html += "<li>No cookies found</li>"
+    print("<p>No cookies found</p>")
 
-html += "</ul><a href='/test/'>Back</a></body></html>"
-
-print(html)
+print('<br><a href="/index.html">Back</a></body></html>')
